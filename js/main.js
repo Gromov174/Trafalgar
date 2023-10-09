9164c4ae3e69297c08c32be22bd127ed7550ea26
Gromov174 <GromovAlexandr174@gmail.com>
2023-10-09 22:12:51 +0500

    90% Complete, beacause not have anchor

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+const burger = document.querySelector('.header__burger')
+const burgerLine = document.querySelector('.header__burger-line')
+const overlay = document.querySelector('.header__nav-list')
+const noScroll  = document.querySelector('.html')
+
+burger.addEventListener('click', (e) => {
+  e.preventDefault()
+  burgerLine.classList.toggle('header__burger-line--active')
+  overlay.classList.toggle('header__nav-list--active')
+  noScroll.classList.toggle('html--noscroll')
+})
+
+
+
+
 const swiper = new Swiper('.testimonials__slider', {
   // Optional parameters
   direction: 'horizontal',
