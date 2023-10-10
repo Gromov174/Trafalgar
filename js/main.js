1db71a426ddcac720cf5326d7475c2837deddfcc
Gromov174 <GromovAlexandr174@gmail.com>
2023-10-10 16:25:05 +0500

    add anchor and disable preventDefault of btn

--- js/main.js
+++ js/main.js
@@ -1,7 +1,40 @@
 const burger = document.querySelector('.header__burger')
 const burgerLine = document.querySelector('.header__burger-line')
 const overlay = document.querySelector('.header__nav-list')
-const noScroll  = document.querySelector('.html')
+const noScroll = document.querySelector('.html')
+const btn = document.querySelectorAll('.btn')
+
+btn.forEach(b => {
+  b.addEventListener('click', (e) => {
+    e.preventDefault()
+  })
+})
+
+document.querySelectorAll('a.anchor-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault()
+    if (overlay.classList.contains('header__nav-list--active') || burgerLine.classList.contains('header__burger-line--active')) {
+      overlay.classList.remove('header__nav-list--active')
+      burgerLine.classList.remove('header__burger-line--active')
+    }
+    const href = this.getAttribute('href').substring(1)
+    const scrollTarget = document.getElementById(href)
+    const topOffset = 0
+    const elemenPosition = scrollTarget.getBoundingClientRect().top
+    const offsetPosition = elemenPosition - topOffset
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: 'smooth',
+    })
+  })
+})
+
+window.scroll({
+  top: 150,
+  left: 0,
+  behavior: 'smooth'
+});
 
 burger.addEventListener('click', (e) => {
   e.preventDefault()
@@ -9,9 +42,6 @@ burger.addEventListener('click', (e) => {
   overlay.classList.toggle('header__nav-list--active')
   noScroll.classList.toggle('html--noscroll')
 })
-
-
-
 
 const swiper = new Swiper('.testimonials__slider', {
   // Optional parameters
@@ -27,10 +57,5 @@ const swiper = new Swiper('.testimonials__slider', {
   navigation: {
     nextEl: '.testimonials__arrow-next',
     prevEl: '.testimonials__arrow-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
   },
 });
