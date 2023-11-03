046b7f59046ead6e28c8c01f0342a32145be21cd
Gromov174 <GromovAlexandr174@gmail.com>
2023-11-04 01:42:36 +0500

    add linter and little bit fix js code

--- js/main.js
+++ js/main.js
@@ -1,70 +1,70 @@
-const burger = document.querySelector('.header__burger')
-burgerLine = document.querySelector('.header__burger-line')
-overlay = document.querySelector('.header__nav-list')
-noScroll = document.querySelector('.html')
-btn = document.querySelectorAll('.btn')
-nonActive = document.querySelectorAll('.disable-link')
-link = document.querySelectorAll('a.anchor-link')
-slider = new Swiper('.testimonials__slider', {
-  direction: 'horizontal',
-  loop: true,
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".header__burger"),
+    burgerLine = document.querySelector(".header__burger-line"),
+    overlay = document.querySelector(".header__nav-list"),
+    noScroll = document.querySelector(".html"),
+    btn = document.querySelectorAll(".btn"),
+    nonActive = document.querySelectorAll(".disable-link"),
+    link = document.querySelectorAll("a.anchor-link"),
+    disablelink = (constName) => {
+      constName.forEach((d) => {
+        d.addEventListener("click", (e) => {
+          e.preventDefault();
+        });
+      });
+    },
+    intClass = (constName, action, className) => {
+      constName.classList[action](className);
+    },
+    slider = new Swiper(".testimonials__slider", {
+      direction: "horizontal",
+      loop: true,
 
-  pagination: {
-    el: '.testimonials__arrow-pagination',
-    clickable: true,
-  },
+      pagination: {
+        el: ".testimonials__arrow-pagination",
+        clickable: true,
+      },
 
-  navigation: {
-    nextEl: '.testimonials__arrow-next',
-    prevEl: '.testimonials__arrow-prev',
-  },
-})
+      navigation: {
+        nextEl: ".testimonials__arrow-next",
+        prevEl: ".testimonials__arrow-prev",
+      },
+    });
 
-function disablelink(constName) {
-  constName.forEach(d => {
-    d.addEventListener('click', (e) => {
-      e.preventDefault()
-    })
-  })
-}
-function intClass(constName, action, className) {
-  constName.classList[action](className)
-}
+  disablelink(btn);
+  disablelink(nonActive);
 
+  link.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      if (overlay.classList.contains("header__nav-list--active")) {
+        intClass(overlay, "remove", "header__nav-list--active");
+        intClass(burgerLine, "remove", "header__burger-line--active");
+        intClass(noScroll, "remove", "html--noscroll");
+      }
+      const href = this.getAttribute("href").substr(1),
+        scrollTarget = document.getElementById(href),
+        elemenPosition = scrollTarget.getBoundingClientRect().top,
+        offsetPosition = elemenPosition - 10;
 
-disablelink(btn)
-disablelink(nonActive)
+      window.scrollBy({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    });
+  });
 
+  window.scroll({
+    top: 150,
+    left: 0,
+    behavior: "smooth",
+  });
 
-link.forEach(link => {
-  link.addEventListener('click', function (e) {
-    e.preventDefault()
-    if (overlay.classList.contains('header__nav-list--active')) {
-      intClass(overlay, 'remove', 'header__nav-list--active')
-      intClass(burgerLine, 'remove', 'header__burger-line--active')
-      intClass(noScroll, 'remove', 'html--noscroll')
-    }
-    const href = this.getAttribute('href').substring(1)
-    const scrollTarget = document.getElementById(href)
-    const elemenPosition = scrollTarget.getBoundingClientRect().top
-    const offsetPosition = elemenPosition - 10
-
-    window.scrollBy({
-      top: offsetPosition,
-      behavior: 'smooth',
-    })
-  })
-})
-
-window.scroll({
-  top: 150,
-  left: 0,
-  behavior: 'smooth'
+  burger.addEventListener("click", (e) => {
+    e.preventDefault();
+    intClass(burgerLine, "toggle", "header__burger-line--active");
+    intClass(overlay, "toggle", "header__nav-list--active");
+    intClass(noScroll, "toggle", "html--noscroll");
+  });
 });
-
-burger.addEventListener('click', (e) => {
-  e.preventDefault()
-  intClass(burgerLine, 'toggle', 'header__burger-line--active')
-  intClass(overlay, 'toggle', 'header__nav-list--active')
-  intClass(noScroll, 'toggle', 'html--noscroll')
-})
